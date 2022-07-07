b43055dce3924802c94d82f4662d10975b216418
jordyreyesj <jordy.2006.13@gmail.com>
2022-07-07 10:48:55 -0500

    final

---
+++ src/main/java/com/nttdata/eurekaserver/EurekaserverApplication.java
@@ -0,0 +1,18 @@
+package com.nttdata.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Eureka server.
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaserverApplication.class, args);
+	}
+
+}
